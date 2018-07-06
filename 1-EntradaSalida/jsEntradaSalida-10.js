dc79608176a3190f138686519e445be2017adde5
lucasbeltran <lucas.beltran@outlook.es>
2018-07-06 19:30:43 -0300

    Ejercicio 10
    Se completa el ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,19 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    var inputImporte;
+    var inputResultado;
+    var porcentajeARestar;
+
+    inputImporte = document.getElementById('importe');
+    inputResultado = document.getElementById('resultado');
+
+    porcentajeARestar = calcularPorcentaje(parseInt(inputImporte.value), 25);
+
+    inputResultado.value = parseInt(inputImporte.value) - porcentajeARestar;
+}
+
+function calcularPorcentaje(importe, porcentaje)
+{
+    return (importe*porcentaje)/100;    
 }
