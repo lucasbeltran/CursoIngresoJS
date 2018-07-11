6cd030492c67108dbb7f70b2abbfa74c0cad0830
lucasbeltran <lucas.beltran@outlook.es>
2018-07-10 21:58:22 -0300

    Ejercicios switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,26 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
-
-//alert (mesDelAño);
-	
-	
-
-
-
-}//FIN DE LA FUNCIÓN
+    var mesDelAño = document.getElementById('mes').value;
+    
+    switch(mesDelAño)
+    {
+        case "Febrero":
+            alert("El mes seleccionado tiene 28 dias");
+            break;
+        case "Abril":
+        case "Junio":
+        case "Septiembre":
+        case "Noviembre":
+            alert("El mes seleccionado tiene 30 dias");
+            break;
+        case "Enero":
+        case "Marzo":
+        case "Mayo":
+        case "Julio":
+        case "Agosto":
+        case "Octubre":
+        case "Diciembre":
+            alert("El mes seleccionado tiene 31 dias");
+            break;
+    }
+}
